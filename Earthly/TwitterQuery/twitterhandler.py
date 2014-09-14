d082ad0fbee4c211d5593d6011c991c04e87417e
Robert Adkins <robertadkins00@gmail.com>
2014-09-14 06:52:48 -0400

    get hashtags from database to query twitter

--- Earthly/TwitterQuery/twitterhandler.py
+++ Earthly/TwitterQuery/twitterhandler.py
@@ -19,7 +19,11 @@ api = TwitterAPI(API_key, API_secret, access_key, access_secret)
 def get_tweets(hashtag_list):
     
     tweetls = api.request('statuses/filter', {'track':hashtag_list})
+    print tweetls.text
     for tweet in tweetls.get_iterator():
+
+        print tweet['text']
+
         if(tweet['coordinates'] != None):
             
             myHashes = ""
