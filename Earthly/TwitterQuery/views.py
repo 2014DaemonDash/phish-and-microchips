ea653a6a1c5e8aeafe5effccde2960379b24c161
kyleheadley <kheadley@umd.edu>
2014-09-14 00:17:06 -0400

    better merge

--- Earthly/TwitterQuery/views.py
+++ Earthly/TwitterQuery/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
 import json
-=======
->>>>>>> e93cf83c73bb1c7c337351dffbf52087db8b4c64
 
 def globe_page_view(request):
     return render(request,'globeTest.html',None)
 
-<<<<<<< HEAD
 
 def leaderboard(request):
     
@@ -19,9 +15,9 @@ def leaderboard(request):
 
 def userscores(request):
     return HttpResponse("unimplemented")
-=======
+
 def test_view(request):
     return HttpResponse("hello world");
 
 
->>>>>>> e93cf83c73bb1c7c337351dffbf52087db8b4c64
+
