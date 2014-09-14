51de44a52a16bd814792ae43de9975ed8b1cf881
Alex <Leaf@Fox-Lab.local>
2014-09-13 22:43:34 -0400

    etc changes

--- Earthly/TwitterQuery/models.py
+++ Earthly/TwitterQuery/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class SearchTerm(models.Model):
     text = models.CharField(max_length = 140)
+    score = models.IntegerField()
+
+class TwitterUser(models.Model):
+    name = models.CharField(max_length = 140)
+    score = models.IntegerField()
