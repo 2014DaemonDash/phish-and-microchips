8a20a94b0951c067425113e35c5fc42c9765ac8c
kyleheadley <kheadley@umd.edu>
2014-09-14 00:08:10 -0400

    Added leaderboard skelleton code

--- Earthly/Earthly/urls.py
+++ Earthly/Earthly/urls.py
@@ -9,4 +9,13 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.globe_page_view),
+    
+    #what do we do here?
+    #url(r'^background/([0-9]{4})/([0-9]{2})/' views.month_archive),
+    
+    #requires: none
+    #returns: json [{username:score},...]
+    url(r'^leaderboard/', views.leaderboard),
+    #requires: ?users='name,name,...'
+    url(r'^scoreuser/', views.userscores),
 )
--- Earthly/TwitterQuery/views.py
+++ Earthly/TwitterQuery/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 def globe_page_view(request):
     return render(request,'globeTest.html',None)
+
+
+def leaderboard(request):
+    
+    #enter the leaders here
+    leaders = {'firstleader':2134213,'secondleader':2134212,}
+    
+    return HttpResponse(json.dumps(leaders))
+
+def userscores(request):
+    return HttpResponse("unimplemented")
