55debf589d19c54e6d83d73b3e80fa35ddbda747
kyleheadley <kheadley@umd.edu>
2014-09-14 01:37:25 -0400

    reset urls

--- Earthly/Earthly/urls.py
+++ Earthly/Earthly/urls.py
@@ -9,5 +9,12 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.globe_page_view),
-    url(r'^test/$', twitterhandler.get_tweets)
+    url(r'^test/$', twitterhandler.get_tweets),
+    
+    #requires: none
+    #returns json object {'name':score,...}
+    url(r'^leaderboard/', views.leaderboard),
+    #requires: ?users='name1,name3,...'
+    #returns: json object {'name1':score,'name3':score,...}
+    url(r'^scoreuser/', views.userscores),
 )
